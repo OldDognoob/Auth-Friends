e7af9968105a3ccc3bca367bcb26d1ac34981555
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-25 19:04:35 +0200

    privateroute-file

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Route, Link, Redirect, Switch } from "react-router-dom";
-import './App.css';
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+//components
+import Login from "./components/Login";
+import Friends from "./components/Friends";
+import FriendForm from "./components/FriendForm";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
+      <nav className="nav">
+        <Link to="/login">Login</Link>
+        <Link to="/protected">Friends</Link>
+        <Link to="addFriend">Add a Friend</Link>
+      </nav>
       <Header />
       <Switch>
-        <Route/>
-        <Route/>
-        <PrivateRoute/>
+        <Route path="/login" component={Login}/>
+        <Route path="/addFriend" component={FriendForm}/>
+        <PrivateRoute path="/protected" component={Friends} />
       </Switch>
     </div>
   );
